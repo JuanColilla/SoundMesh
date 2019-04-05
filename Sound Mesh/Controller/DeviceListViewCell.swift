0d4c1ac965bf564842b46ec089c07583b237186f
Juan Colilla <neodefensio@icloud.com>
2019-04-05 02:45:15 +0200

    Added navigation controllers, designed MeshManagement View and respective ViewController, linked views, added constraints for MeshManagement View and added the TableView, TableViewCell and a border for the TableView.

---
+++ Sound Mesh/Controller/DeviceListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DeviceListViewCell.swift
+//  Sound Mesh
+//
+//  Created by Juan Colilla on 05/04/2019.
+//  Copyright © 2019 Juan Colilla. All rights reserved.
+//
+
+import UIKit
+
+class DeviceListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
